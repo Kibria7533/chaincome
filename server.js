4fa0180c089151c22e5c8b259602df566acaf608
kibria7533 <tenminuteversity@gmail.com>
2020-12-03 22:52:51 +0600

    db add

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cor=require('cors');
 const mongoose=require('mongoose');
 require('dotenv').config()
 
-mongoose.connect('mongodb://localhost:27017/trippledev',{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true});
 
 var db=mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
